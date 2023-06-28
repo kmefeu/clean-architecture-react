45e7e51c0c543addd0acd3f892a317c5b156de6b
Matheus Corrêa <mcorreadesign@gmail.com>
2023-06-27 23:03:32 -0300

    feat: Authentication UseCase receives repositories interfaces

--- src/feature/authentication/domain/use-case/authentication.ts
+++ src/feature/authentication/domain/use-case/authentication.ts
@@ -1,6 +1,7 @@
 import { UseCase } from "../../../../core/domain/use-case/use-case";
 import { AppError } from "../../../error/domain/entity/appError";
 import { AuthenticationEntity } from "../entity/authentication-entity";
+import { AuthenticationRepositoriesInterface } from "../repositories-interface/authentication-repositories-interface";
 
 export interface AuthenticationParams {}
 
@@ -8,7 +9,14 @@ export class Authentication extends UseCase<
   AuthenticationEntity,
   AuthenticationParams
 > {
+  repositoriesInterface: AuthenticationRepositoriesInterface;
+
+  constructor(repositoriesInterface: AuthenticationRepositoriesInterface) {
+    super();
+    this.repositoriesInterface = repositoriesInterface;
+  }
+
   call(): AuthenticationEntity | AppError {
-    return new Error();
+    return this.repositoriesInterface.auth();
   }
 }
